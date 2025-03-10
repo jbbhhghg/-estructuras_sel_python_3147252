12c253c631b953b35e471e7e92611a1c0a3d1e7a
Angela <lualgual33@gmail.com>
2025-03-10 07:07:41 -1200

    operador not

---
+++ operadores_logicos.py
@@ -0,0 +1,12 @@
+'''
+operadores logicos
+ 
+ Aquellos que operan unicamente 
+ con valores booleanos (v f)
+ Acorde a las tablas de Verdad
+'''
+
+#Ejemplo 1 Operador not:
+y = not True
+print ("el valor o resultado de operar con not es", y) 
+ 
