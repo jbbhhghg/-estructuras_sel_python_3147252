ee17dbe61c86bf713cb542fb9bcd774104144543
Angela <lualgual33@gmail.com>
2025-03-10 09:46:21 -1200

    operador or

--- operadores_logicos.py
+++ operadores_logicos.py
@@ -11,5 +11,12 @@ y = not True
 print ("el valor o resultado de operar con not es", y) 
  
  # ejemplo: 2 operador and
-y = True and True
+y = False and True
 print ("el resultado de operar con and es", y)
+
+
+#ejemplo 3 :operador or 
+y = False or True
+print ("El resultado de operar con or es", y)
+
+
